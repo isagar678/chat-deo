1396a02531d633aa16992a4d2ab340f89433e556
Sagar Bharati <bharatisagar678@gmail.com>
2025-07-30 19:27:13 +0530

    minor change

--- server/src/modules/chat/chat.gateway.ts
+++ server/src/modules/chat/chat.gateway.ts
@@ -68,7 +68,7 @@ export class ChatGateway
   }
 
   async handleConnection(client: any, ...args: any[]) {
-    const token_received = client.handshake.headers?.authorization?.split(' ')[1];
+    const token_received = client.handshake.auth?.token;
     try {
       const data = this.authService.verifySocketToken(token_received);
       client.userId = data?.userId;
