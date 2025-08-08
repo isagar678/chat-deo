b5c2ad18405dd626657d3d2cabc3f91b3499b293
Sagar Bharati <bharatisagar678@gmail.com>
2025-08-08 18:47:43 +0530

    dependency issue fixed

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../../entities/user.entity';
+import { Chats } from 'src/entities/chat.entity';
+import { FriendShip } from 'src/entities/friendship.entity';
 
 @Module({
   controllers: [UserController],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User,Chats,FriendShip])],
   providers: [UserService],
   exports: [UserService],
 })
